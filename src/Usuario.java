c756c817387c56bb5411cdbbf7b0b98995148346
FilipeBMA <fps32676@gmail.com>
2025-09-07 16:34:35 -0300

    Create Usuario.java

---
+++ src/Usuario.java
@@ -0,0 +1,21 @@
+
+public class Usuario {
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Usuario(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public String getNome() { return nome; }
+    public String getEmail() { return email; }
+    public String getTelefone() { return telefone; }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + ", Email: " + email + ", Telefone: " + telefone;
+    }
+}
